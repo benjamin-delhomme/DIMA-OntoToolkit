e951b4bb2017933b23dae42ae51b69c8f7faac0b
benjamin-delhomme <benjamin.delhomme@free.fr>
2025-07-24 15:12:20 +0300

    Add all the techniques from the tactic TA0033 - Content Exposure (Exposition de contenus)

--- src/dima_otk/bias_analysis/memorise/memorise_logic.py
+++ src/dima_otk/bias_analysis/memorise/memorise_logic.py
@@ -17,10 +17,15 @@ from typing import List, Dict, Callable
 
 from dima_otk.utils.cache import load_or_compute_cache
 
+from .te0331_extractor import extract_technique_te0331
+from .te0332_extractor import extract_technique_te0332
+from .te0333_extractor import extract_technique_te0333
 
 def get_memorise_techniques(processed_article: dict) -> dict:
     return {
-        # "xxxxxxxxxx": get_memorise_technique(processed_article,"texxxx",extract_technique_texxxx),
+        "RecencyEffect": get_memorise_technique(processed_article,"te0331",extract_technique_te0331),
+        "MereExposureEffect": get_memorise_technique(processed_article,"te0332",extract_technique_te0332),
+        "PrimacyEffect": get_memorise_technique(processed_article,"te0333",extract_technique_te0333),
         # Add more techniques as needed
     }
 
